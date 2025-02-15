8d4b4f661241043a0f1a80dbed0c1945497f52e1
Yog Tandel <152771500+YogTandel@users.noreply.github.com>
2025-02-15 08:32:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "https://doms-backend.vercel.app",
-  "https://doms-site.vercel.app",
+  "https://doms-roan.vercel.app",
 ];
 
 app.use(
